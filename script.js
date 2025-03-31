d037134e54fdcbb4c7dd2fa43b1b9c292dcdbea8
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-03-31 09:10:33 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let nome = prompt("Digite seu primeiro nome");
 let idade = parseInt(prompt("Digite qual será sua idade ao final desse ano"));
 
-alert('Nome: '+nome+'\nIdade: '+idade+'\nData de Nascimento: '+(2025-idade));
+alert('Nome: '+nome+'\nIdade ao final do ano: '+idade+'\nData de Nascimento: '+(2025-idade));
