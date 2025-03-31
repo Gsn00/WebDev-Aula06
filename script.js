31095b45345f035bc0ca8f87d4d76417adad3104
Gabriel Novais <labsfiap@fiap.com.br>
2025-03-31 09:05:42 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let nome = prompt("Digite seu primeiro nome");
+let idade = parseInt(prompt("Digite qual será sua idade ao final desse ano"));
+
+alert('Nome: '+nome+'\nIdade: '+idade+'\nData de Nascimento: '+(2025-idade));
